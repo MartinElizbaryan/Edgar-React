1377f42234928d3bb636335bbf9fa9c2edfe0f8a
EdgarHambardzumyan <edhambardzumyan666803@gmail.com>
2024-04-11 16:54:07 +0400

    qwe

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { PLACEHOLDER_URL, QUERY_TYPE } from "./constants/common";
 
 const App = () => {
   const [data, setData] = useState([]);
-  const [countData, setCountData] = useState(0);
   // const [dataType, setDataType] = useState(QUERY_TYPE.users);
-
-  const addCountData = () => setCountData(countData + 1);
 
   useEffect(() => {
     getData();
   }, []);
+
+  const getRandomUser = ()=>{
+
+  }
 
   const getData = async () => {
     const users = await getUsers();
@@ -23,28 +24,32 @@ const App = () => {
 
   return (
     <div>
-      <button onClick={addCountData}>Add Count</button>
-      {/* <button onClick={selectUsers}>Users</button> */}
-      {/* <button onClick={selectTodos}>Todos</button> */}
-
-      <h1>Count: {countData}</h1>
-
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
+            <th>Person</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {data.map((user) => (
             <tr key={user.id}>
-              <td>{user.getName()}</td>
-              <td>{user.getEmail()}</td>
-              <td>{user.getPhone()}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td></td>
+              <td>
+                <button>Done green</button>
+                <button>Delete</button>
+              </td>
             </tr>
           ))}
+          <tr>
+            <td>
+              <button onClick={}>Add Random Task</button>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
